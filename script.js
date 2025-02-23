df03c7767d79b948cab2250f1a5fc81bce31f015
MutukuBill <62650566+MutukuBill@users.noreply.github.com>
2025-02-23 18:16:14 +0300

    JS

--- script.js
+++ script.js
@@ -106,3 +106,9 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("visit-counter").textContent = `Visits: ${visitCount}`;
 });
 
+
+window.addEventListener("scroll", function() {
+            let scrollPosition = window.scrollY;
+            let scaleValue = 1 + scrollPosition * -0.0005; // Adjust zoom speed
+            document.querySelector(".parallax-image").style.transform = `scale(${scaleValue}) translateY(${scrollPosition * 0.5}px)`;
+        });
